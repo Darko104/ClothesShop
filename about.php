86cb227116e89bdb65d7f14ad9e4f7cad629e50e
Darko <darko.dj212@gmail.com>
2022-07-25 17:41:42 +0200

    Changed 'About' page info

--- about.php
+++ about.php
@@ -26,8 +26,8 @@
                     <p id="author-ocupation">Web developer</p>
                 </div>
                 <div id="author-card-social">
-                    <i class="fab fa-linkedin"></i>
-                    <i class="fab fa-github"></i>
+                    <a href="https://www.linkedin.com/in/darko-%C4%91uki%C4%87-568337246/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/darko104" target="_blank"><i class="fab fa-github"></i></a>
                 </div>
             </div>
         </div>
@@ -35,7 +35,9 @@
             <div id="initial-author-info">
                 <h1>About me<span id="author-header-colored">!</span></h1>
                 <p id="author-basic-info">My name is Darko Đukić, i am a Web Developer from Pančevo, Serbia.</p>
-                <p id="author-more-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sequi dolor iure perferendis! Vitae inventore aliquid ratione ipsa excepturi, voluptatibus aspernatur!<br><br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati laboriosam impedit dolorum hic pariatur cumque perferendis.</p>
+                <div id="author-buttons">
+                    <a href="http://darkodjwebdev.com/" target="_blank" class="author-button">My portfolio</a>
+                </div>
             </div>
         </div>
     </div>
